db62dfcdc6e9f2d70a3c799423afb3a6be398994
Mateus dos Santos de Andrade <63256486+Mateus987@users.noreply.github.com>
2025-11-19 11:28:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+(function() {
+    if (window.location.pathname === "/checkout/cart") {
 
-    const phrase = document.getElementById('phrase');
+        let chApp = document.getElementById("ch-app");
 
-    const text = [ "\"O coração tem razões que a própria razão desconhece\" -  Blaise Pascal",
-                    "\"Penso, logo existo\" -  René Descartes", 
-                    "\"A felicidade é a única coisa que podemos dar sem possuir\" -  Voltaire",
-                    "\"A verdadeira sabedoria está em reconhecer a própria ignorância\" -  Sócrates",
-                    "\"Somos o que fazemos repetidamente. A excelência, então, não é um ato, mas um hábito\" -  Aristóteles",
-                    "\"A vida pode ser compreendida olhando-se para trás, mas só pode ser vivida olhando-se para frente\" -  Søren Kierkegaard",
-                    "\"A mente é como um paraquedas, só funciona se estiver aberta\" -  Albert Einstein",
-                    "\"A vida é realmente simples, mas insistimos em torná-la complicada\" -  Confúcio",
-                    "\"O saber a gente aprende com os mestres e os livros. A sabedoria se aprende é com a vida e com os humildes\" -  Cora Coralina",
-                    "\"Feliz aquele que transfere o que sabe e aprende o que ensina\" -  Cora Coralina",
-                    "\"Se algo está além do seu controle, por que se preocupar com isso?\" -  Marco Aurélio",
-                    "\"O conhecimento só é verdadeiramente nosso quando é construído por nós\" -  Immanuel Kant"
-                    ];
+        if (!chApp) {
+            chApp = document.createElement("div");
+            chApp.id = "ch-app";
+            document.body.appendChild(chApp);
+        }
 
-    var i = Math.floor(text.length * Math.random());
-    phrase.innerText = text[i];
-});
+        chApp.style.backgroundColor = "blueviolet";
+        chApp.style.width = "100%";
+        chApp.style.height = "100px";
+        chApp.style.display = "block";
 
-console.log("testando script de disparo de eventos na página de produtos");
-
-const buttonbuy = document.querySelector("#button-buy");
-buttonbuy.addEventListener('mouseover', function(){
-    console.log("Vai comprar?");
-} )
+        console.log("Elemento #ch-app criado/estilizado com sucesso.");
+    }
+})();
