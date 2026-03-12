aa2a1dfb8323dee1c6db46faa2c54a92ef9ce726
Mateus dos Santos de Andrade <63256486+Mateus987@users.noreply.github.com>
2026-03-12 16:12:38 -0300

    Update tray.js

--- tray.js
+++ tray.js
@@ -1,4 +1,22 @@
-(function() {
+(function () {
+
+    const currentScript = document.currentScript;
+
+    // Se o script ainda não for module, recria ele
+    if (currentScript && currentScript.type !== "module") {
+
+        const newScript = document.createElement("script");
+        newScript.type = "module";
+        newScript.src = currentScript.src;
+
+        currentScript.parentNode.insertBefore(newScript, currentScript);
+        currentScript.remove();
+
+        console.log("Recarregando script como module...");
+        return; // impede execução duplicada
+    }
+
+    // Seu código original
     if (window.location.pathname === "/checkout/cart") {
 
         let chApp = document.getElementById("ch-app");
@@ -16,4 +34,5 @@
 
         console.log("Elemento #ch-app criado/estilizado com sucesso.");
     }
+
 })();
